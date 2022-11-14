55cb9f6af3aa05951dbb8d18217f3f38f57a1df8
Gabriel Vitor <gabrielvinces2@gmail.com>
2022-11-14 12:26:33 -0300

    Project created

---
+++ backend/dsMeta/src/test/java/com/dossa/dsMeta/DsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dossa.dsMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
